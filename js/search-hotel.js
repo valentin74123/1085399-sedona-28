db11e6f3ce7e5c8ef2c6f5274ad1c43f8911d001
valentin74123 <58230964+valentin74123@users.noreply.github.com>
2020-07-07 21:58:59 +0500

    Исправление ошибок

--- js/search-hotel.js
+++ js/search-hotel.js
@@ -1,15 +1,12 @@
+var searchLink = document.querySelector(".button-interested-in");
+var searchPopupForm = document.querySelector(".modal-date");
+var isFormOpen = false; 
+var searchForm = searchPopupForm.querySelector(".date-form");
+var arrivalDate= searchPopupForm.querySelector(".search-arrival-date");
+var departureDate = searchPopupForm.querySelector(".search-departure-date");
 
-
-let searchLink = document.querySelector(".button-interested-in");
-let searchPopupForm = document.querySelector(".modal-date");
-let isFormOpen = false; 
-let searchForm = searchPopupForm.querySelector(".date-form");
-let arrivalDate= searchPopupForm.querySelector(".search-arrival-date");
-let departureDate = searchPopupForm.querySelector(".search-departure-date");
-
-
-let isStorageSupport = true;
-let storage = "";
+var isStorageSupport = true;
+var storage = "";
 
 try {
   storage = localStorage.getItem("arrival-date");
@@ -18,38 +15,37 @@ try {
   isStorageSupport = false;
 }
 
-
 searchLink.addEventListener("click", function (evt) {
-    if (isFormOpen === true){
-        searchPopupForm.classList.remove("modal-show");
-        searchPopupForm.classList.remove("modal-error");
-        isFormOpen = false;
-    } else {
-        searchPopupForm.classList.add("modal-show");
-        arrivalDate.focus();
-        isFormOpen = true;  
-    }
+  if (isFormOpen === true){
+    searchPopupForm.classList.remove("modal-show");
+    searchPopupForm.classList.remove("modal-error");
+    isFormOpen = false;
+  } else {
+    searchPopupForm.classList.add("modal-show");
+    arrivalDate.focus();
+    isFormOpen = true;  
+  }
 });
 
 searchForm.addEventListener("submit", function (evt) {
-    if (!arrivalDate.value || !departureDate.value) {
+  if (!arrivalDate.value || !departureDate.value) {
     evt.preventDefault();
     searchPopupForm.classList.remove("modal-error");
     searchPopupForm.offsetWidth = searchPopupForm.offsetWidth;
     searchPopupForm.classList.add("modal-error");
-    } else {
-        if (isStorageSupport) {
-            localStorage.setItem("arrival-date", arrivalDate.value);
-            localStorage.setItem("departure-date", departureDate.value);
-        }
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("arrival-date", arrivalDate.value);
+      localStorage.setItem("departure-date", departureDate.value);
     }
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (searchPopupForm.classList.contains("modal-show")) {
-        evt.preventDefault();
-        searchPopupForm.classList.remove("modal-show");
-      }
+  if (evt.keyCode === 27) {
+    if (searchPopupForm.classList.contains("modal-show")) {
+      evt.preventDefault();
+      searchPopupForm.classList.remove("modal-show");
     }
+  }
 });
